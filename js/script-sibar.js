c6281454737b5eb77235e27249ab13062a147a68
Gimena Riveros <72352905+Gimena-Riveros@users.noreply.github.com>
2026-04-23 14:00:15 -0300

    Delete js/script-sibar.js
    Rename script-sibar to side.js. MGR

--- js/script-sibar.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.querySelector('.sidebar');
-const toggleBTNmenu = document.querySelector('.toggle-btn-menu');
-
-toggleBTNmenu.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-} );
